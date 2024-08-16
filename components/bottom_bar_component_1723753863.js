538968cbad74742fdbb29720bec30a0a436106ac
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-16 17:01:24 +0000

    Merge code from frontend

--- components/bottom_bar_component_1723753863.js
+++ components/bottom_bar_component_1723753863.js
@@ -3,14 +3,7 @@ It has two buttons: Contact button that directs to the contact page and Pay butt
 */
 Vue.component("bottom_bar_component_1723753863", {
     template: `
-    <section id="bottom-bar-component" class="fixed bottom-0 z-30 w-full lg:w-1/2 !bg-opacity-80 backdrop-blur-sm" :class="bottomBarBackgroundStyle">
-        <div id="bottom-bar-component-content" class="w-full max-w-xl mx-auto px-4 sm:px-6">
-            <div id="action-buttons" class="flex py-4 md:py-6 space-x-4 justify-center">
-                <a id="contact-button" class="btn flex items-center justify-center w-1/2 h-12 p-3" :class="[contactButtonStyle, contactButtonHoverStyle, shadowStyle, 'transition duration-200 ease-in-out']" href="contact.html">Contact</a>
-                <a id="pay-button" class="btn flex items-center justify-center w-1/2 h-12 p-3 animate-shine bg-[size:200%_100%] hover:bg-[image:none] transition duration-200 ease-in-out" :class="[payButtonTextStyle, payButtonBackgroundStyle, shadowStyle, payButtonGradientStyle]">Pay</a>
-            </div>
-        </div>
-    </section>
+    <section id="bottom-bar-component" class="fixed bottom-0 z-30 w-full lg:w-1/2 !bg-opacity-80 backdrop-blur-sm bg-white dark:bg-slate-950" style=""><div id="bottom-bar-component-content" class="w-full max-w-xl mx-auto px-4 sm:px-6"><div id="action-buttons" class="flex py-4 md:py-6 space-x-4 justify-center" style=""><a id="contact-button" href="contact.html" class="btn flex items-center justify-center w-1/2 h-12 p-3 text-slate-900 dark:text-slate-200 dark:bg-slate-900 rounded-lg hover:border-slate-300 dark:border-slate-800 dark:hover:border-slate-700 border shadow-lg transition duration-200 ease-in-out">Contact</a> <a id="pay-button" class="btn flex items-center justify-center w-1/2 h-12 p-3 animate-shine bg-[size:200%_100%] hover:bg-[image:none] transition duration-200 ease-in-out text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg bg-[linear-gradient(100deg, theme(colors.blue.500), 45%, theme(colors.blue.400), 55%, theme(colors.blue.500))] rounded-lg" style="" data-mf-new="true">Dont Pay</a></div></div></section>
     `,
     data() {
         return {
